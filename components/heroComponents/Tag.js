6a16b361365aca5dc05be1bd29e2b3360b045017
Angelina Truong <angelinatruong@Angelinas-MacBook-Air.local>
2023-02-04 20:40:20 -0800

    update title

--- components/heroComponents/Tag.js
+++ components/heroComponents/Tag.js
@@ -1,7 +1,7 @@
 const Tag = ({ category, year }) => {
   return (
     <div className="border-l border-gray-900 px-3 pt-1 mb-10">
-      <p className="text-xs tracking-wider m-0 leading-tight">{category}</p>
+      <p className="text-s tracking-wider m-0 leading-tight">{category}</p>
       { year && <p className="text-xs tracking-wider m-0 leading-tight">{year}</p>}
     </div>
   )
