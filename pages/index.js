0ae88245db06835cc983fc0b79cba40efa0ec9a0
Angelina Truong <angelinatruong@Angelinas-MacBook-Air.local>
2023-02-04 20:44:08 -0800

    change title size

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ const Home = ({ inventory = [], categories = [] }) => {
         flex lg:flex-row flex-col"
         >
           <div className="pt-4 pl-2 sm:pt-12 sm:pl-12 flex flex-col">
-            <Tag year="2023" category="MY PARTY CITY" />
+            <Tag year="2023" category="MY PARTY PLACE" />
             <Center
               price="10"
               title={inventory[4].name}
